b10e2bf422bb55c20d7a0611996057a505f72e77
geor0014 <geor0014@hz.nl>
2023-06-06 12:54:42 +0200

    light refactoring in app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const fs  = require('fs');
 const express = require('express');
+const { get } = require('http');
 
 const app = express();
 // middleware
@@ -10,7 +11,7 @@ const port = process.env.PORT || 3000;
 //  we read the file here because we want to read it only once when the server starts and not every time we make a request
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
-app.get('/api/v1/tours', (req,res)=>{
+const getAllTours = (req,res)=>{
     res.status(200).json({
         status: 'success',
         results: tours.length,
@@ -19,10 +20,28 @@ app.get('/api/v1/tours', (req,res)=>{
             tours
         }
     })
-})
+}
 
+const getTour = (req,res)=>{
+    // convert the string to a number
+    const id = req.params.id * 1
 
-app.post('/api/v1/tours', (req,res) =>{
+    if (id > tours.length){
+        return res.status(404).json({
+            status:"unsuccessful",
+            message:"Invalid ID"
+        })
+    }
+    // find creates a new array with the elements that pass the test
+    const tour  = tours.find(t => t.id===id)
+
+    res.status(200).json({
+        status:"success",
+        tour
+    })
+}
+
+const createTour = (req,res) =>{
     const newId = tours[tours.length-1].id + 1
     const newTour = Object.assign({id:newId}, req.body)
 
@@ -35,8 +54,41 @@ app.post('/api/v1/tours', (req,res) =>{
             }
         })
     })
-})
+}
 
+const updateTour = (req,res)=>{
+    if (req.params.id * 1 > tours.length){
+        return res.status(404).json({
+            status:"unsuccessful",
+            message:"Invalid ID"
+        })
+    }
+
+   res.status(200).json({
+         status:"success",
+            data:{
+                tour:"<Updated tour here...>"
+            }
+   })
+
+}
+
+const deleteTour = (req,res)=>{
+    if (req.params.id * 1 > tours.length){
+        return res.status(404).json({
+            status:"unsuccessful",
+            message:"Invalid ID"
+        })
+    }
+
+    res.status(204).json({
+        status:"success",
+        data:null
+    })
+}
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour)
+app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour)
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
