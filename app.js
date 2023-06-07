d51c69797a04c4a3a4feefcf67e53e7f22662716
geor0014 <geor0014@hz.nl>
2023-06-07 12:38:43 +0200

    refactor forder structure, add server.js and /controllers /routes

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const fs  = require('fs');
 const express = require('express');
 const morgan = require('morgan')
 
@@ -6,131 +5,11 @@ const app = express();
 //////////////////////////// middleware
 app.use(morgan('dev'))
 app.use(express.json())
-
-const port = process.env.PORT || 3000; 
-
-//  we read the file here because we want to read it only once when the server starts and not every time we make a request
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
-
-const tourRouter = express.Router()
-const userRouter = express.Router()
-
-// '//////////////////Route handlers 
-const getAllTours = (req,res)=>{
-    res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            // we can just write tours because it is the same as tours: tours
-            tours
-        }
-    })
-}
-
-const getTour = (req,res)=>{
-    // convert the string to a number
-    const id = req.params.id * 1
-
-    if (id > tours.length){
-        return res.status(404).json({
-            status:"unsuccessful",
-            message:"Invalid ID"
-        })
-    }
-    // find creates a new array with the elements that pass the test
-    const tour  = tours.find(t => t.id===id)
-
-    res.status(200).json({
-        status:"success",
-        tour
-    })
-}
-
-const createTour = (req,res) =>{
-    const newId = tours[tours.length-1].id + 1
-    const newTour = Object.assign({id:newId}, req.body)
-
-    tours.push(newTour)
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status:"success",
-            data:{
-                tour:newTour
-            }
-        })
-    })
-}
-
-const updateTour = (req,res)=>{
-    if (req.params.id * 1 > tours.length){
-        return res.status(404).json({
-            status:"unsuccessful",
-            message:"Invalid ID"
-        })
-    }
-
-   res.status(200).json({
-         status:"success",
-            data:{
-                tour:"<Updated tour here...>"
-            }
-   })
-
-}
-
-const deleteTour = (req,res)=>{
-    if (req.params.id * 1 > tours.length){
-        return res.status(404).json({
-            status:"unsuccessful",
-            message:"Invalid ID"
-        })
-    }
-
-    res.status(204).json({
-        status:"success",
-        data:null
-    })
-}
-
-const getAllUsers = (req,res)=>{
-    res.status(500).json({
-        status:'error'
-    })
-}
-const getUser = (req,res)=>{
-    res.status(500).json({
-        status:'error'
-    })
-}
-const createUser = (req,res)=>{
-    res.status(500).json({
-        status:'error'
-    })
-}
-const deleteUser = (req,res)=>{
-    res.status(500).json({
-        status:'error'
-    })
-}
-const updateUser = (req,res)=>{
-    res.status(500).json({
-        status:'error'
-    })
-}
-
-
-// ////////////////////////////// Routes
-
-tourRouter.route('/').get(getAllTours).post(createTour)
-tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour)
-
-userRouter.route('/').get(getAllUsers).post(createUser)
-userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser)
+/////////////////////////// routers 
+const userRouter = require('./routes/userRoutes')
+const tourRouter = require('./routes/tourRoutes')
 
 app.use('/api/v1/tours',tourRouter)
 app.use('/api/v1/users',userRouter)
 
-//////////////////////////////////Server
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+module.exports = app;
---
+++ controllers/tourController.js
@@ -0,0 +1,79 @@
+const fs  = require('fs');
+//  we read the file here because we want to read it only once when the server starts and not every time we make a request
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`))
+
+exports.getAllTours = (req,res)=>{
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            // we can just write tours because it is the same as tours: tours
+            tours
+        }
+    })
+}
+
+exports.getTour = (req,res)=>{
+    // convert the string to a number
+    const id = req.params.id * 1
+
+    if (id > tours.length){
+        return res.status(404).json({
+            status:"unsuccessful",
+            message:"Invalid ID"
+        })
+    }
+    // find creates a new array with the elements that pass the test
+    const tour  = tours.find(t => t.id===id)
+
+    res.status(200).json({
+        status:"success",
+        tour
+    })
+}
+
+exports.createTour = (req,res) =>{
+    const newId = tours[tours.length-1].id + 1
+    const newTour = Object.assign({id:newId}, req.body)
+
+    tours.push(newTour)
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status:"success",
+            data:{
+                tour:newTour
+            }
+        })
+    })
+}
+
+exports.updateTour = (req,res)=>{
+    if (req.params.id * 1 > tours.length){
+        return res.status(404).json({
+            status:"unsuccessful",
+            message:"Invalid ID"
+        })
+    }
+
+   res.status(200).json({
+         status:"success",
+            data:{
+                tour:"<Updated tour here...>"
+            }
+   })
+
+}
+
+exports.deleteTour = (req,res)=>{
+    if (req.params.id * 1 > tours.length){
+        return res.status(404).json({
+            status:"unsuccessful",
+            message:"Invalid ID"
+        })
+    }
+
+    res.status(204).json({
+        status:"success",
+        data:null
+    })
+}
