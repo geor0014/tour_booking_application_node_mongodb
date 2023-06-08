eeaf887dd54f0fbe08a64cd02b58a67765b92d87
geor0014 <geor0014@hz.nl>
2023-06-08 19:45:49 +0200

    adding eslint and prettier to the project

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const express = require('express');
-const morgan = require('morgan')
+const morgan = require('morgan');
 
 const app = express();
 //////////////////////////// middleware
 // this if statement is for development mode only
-if(process.env.NODE_ENV === 'development'){
-    // morgan is a middleware that logs the request to the console
-    app.use(morgan('dev'))
+if (process.env.NODE_ENV === 'development') {
+  // morgan is a middleware that logs the request to the console
+  app.use(morgan('dev'));
 }
-app.use(express.json())
-app.use(express.static(`${__dirname}/public`))
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
-/////////////////////////// routers 
-const userRouter = require('./routes/userRoutes')
-const tourRouter = require('./routes/tourRoutes')
+/////////////////////////// routers
+const userRouter = require('./routes/userRoutes');
+const tourRouter = require('./routes/tourRoutes');
 
-app.use('/api/v1/tours',tourRouter)
-app.use('/api/v1/users',userRouter)
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-const env = require('dotenv').config({path:'./config.env'})
-const app = require('./app')
-const port = process.env.PORT || 3000; 
+const env = require('dotenv');
 
+env.config({ path: './config.env' });
+
+const app = require('./app');
+
+const port = process.env.PORT || 3000;
 
 //////////////////////////////////Server
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
