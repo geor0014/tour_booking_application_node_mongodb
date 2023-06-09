ae8c8d121242a54ce3ec4142e2fbfc996c5484e9
geor0014 <geor0014@hz.nl>
2023-06-09 22:46:16 +0200

    add more properties to tourModel.js

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,15 +6,52 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true, // remove all white spaces in the beginning and end of the string
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a maxGroupSize'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true, // remove all white spaces in the beginning and end of the string
+    required: [true, 'A tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true, // remove all white spaces in the beginning and end of the string
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a imageCover'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
