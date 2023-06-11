cf195762852ea1f00ff5023d7b614b69a6d4b5fe
geor0014 <geor0014@hz.nl>
2023-06-11 18:54:53 +0200

    add error handling for invalid ID

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,5 @@
+const AppError = require('../utils/appError');
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -24,6 +26,11 @@ const sendErrorProd = (err, res) => {
   }
 };
 
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500; // if err.statusCode is undefined, then set it to 500
   err.status = err.status || 'error'; // if err.status is undefined, then set it to 'error'
@@ -31,6 +38,11 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = {
+      ...err,
+    };
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+
+    sendErrorProd(error, res);
   }
 };
