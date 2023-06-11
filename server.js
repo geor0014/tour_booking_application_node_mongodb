26f6db37426d214388c6e666bd0fd9246d917ffe
geor0014 <geor0014@hz.nl>
2023-06-11 19:12:47 +0200

    add error handling for uncaught exceptions in server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const mongoose = require('mongoose');
 
+// Handle uncaught exceptions globally (e.g. undefined variable) - this is a safety net
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! ⚠️⚠️ Shutting down...');
+  console.log(err);
+  // exit the application
+  process.exit(1);
+});
+
 const env = require('dotenv');
 env.config({ path: './config.env' });
 const app = require('./app');
@@ -24,7 +32,7 @@ const server = app.listen(port, () => {
 // Handle unhandled promise rejections globally (e.g. DB connection error) - this is a safety net
 process.on('unhandledRejection', err => {
   console.log('UNHANDLED REJECTION! ⚠️⚠️ Shutting down...');
-  console.log(err.name, err.message);
+  console.log(err);
   server.close(() => {
     // exit the application
     process.exit(1); // 0 = success, 1 = uncaught exception
