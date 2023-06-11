faf923887aef54619041c9c0400512c97dedd299
geor0014 <geor0014@hz.nl>
2023-06-11 13:31:35 +0200

    add virtual property to tourModel.js

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,57 +1,68 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-    trim: true, // remove all white spaces in the beginning and end of the string
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true, // remove all white spaces in the beginning and end of the string
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a maxGroupSize'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true, // remove all white spaces in the beginning and end of the string
+      required: [true, 'A tour must have a summary'],
+    },
+    description: {
+      type: String,
+      trim: true, // remove all white spaces in the beginning and end of the string
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a imageCover'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    startDates: [Date],
   },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration'],
+  {
+    toJSON: { virtuals: true }, // when data is outputted as JSON (e.g. when we send data to the client), we want the virtual properties to be included
+    toObject: { virtuals: true }, // when data is outputted as an object (e.g. when we console.log the data), we want the virtual properties to be included
   },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a maxGroupSize'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty'],
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    trim: true, // remove all white spaces in the beginning and end of the string
-    required: [true, 'A tour must have a summary'],
-  },
-  description: {
-    type: String,
-    trim: true, // remove all white spaces in the beginning and end of the string
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a imageCover'],
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  startDates: [Date],
+);
+
+// we need to use a function here because we need the this keyword
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7; // this refers to the current document
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
