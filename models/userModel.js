671f2e4a585a7036f00114856a784a3332175b3c
geor0014 <geor0014@hz.nl>
2023-06-12 14:41:03 +0200

    add user model to project

---
+++ models/userModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A user must have a name'],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'A user must have an email'],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'A user must have a password'],
+    minLength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on CREATE and SAVE!!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!',
+    },
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
