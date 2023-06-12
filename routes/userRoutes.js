8b6166353cbf45870fe25beb88aaf27c753bce06
geor0014 <geor0014@hz.nl>
2023-06-12 17:53:46 +0200

    create new user and add authController.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,19 @@
-const express = require('express')
-const userController = require('./../controllers/userController')
-const router = express.Router()
+const express = require('express');
+const userController = require('./../controllers/userController');
+const router = express.Router();
+const authController = require('./../controllers/authController');
 
-router.route('/').get(userController.getAllUsers).post(userController.createUser)
-router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser)
+router
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
 
-module.exports = router
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
+
+router.route('/signup').post(authController.signup);
+
+module.exports = router;
