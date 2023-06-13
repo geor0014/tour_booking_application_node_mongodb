c9f32236a3aa9319dd54e45dbf54b2db362124ed
geor0014 <geor0014@hz.nl>
2023-06-13 21:33:32 +0200

    implement simple nested routes for reviews and tours in tourRoutes.js

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -15,6 +15,10 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
 });
 
 exports.createReview = catchAsync(async (req, res, next) => {
+  // if there is no tour or user in the body, then we will add it to the body
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+
   const newReview = await Review.create({
     review: req.body.review,
     rating: req.body.rating,
